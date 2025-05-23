7641c1a6d8b52912b11f07bd14bebde2831617f0
Fulgurance <zohran.londais@gmail.com>
2025-05-23 16:31:32 +0100

    Updated inheritance

--- Kernel-Sources/0.2.0/0.2.0.cr
+++ Kernel-Sources/0.2.0/0.2.0.cr
@@ -1,11 +1,2 @@
-class Target < ISM::ComponentSoftware
-
-    def install
-        super
-
-        recordSelectedKernel
-        updateKernelSymlinks
-        updateKernelOptionsDatabase
-    end
-
+class Target < ISM::KernelSourcesSoftware
 end
